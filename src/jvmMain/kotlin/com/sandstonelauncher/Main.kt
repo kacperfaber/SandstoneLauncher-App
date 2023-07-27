5775b28de9509568617891885732ebadc96b4592
Kacper Faber <kacperf1234@gmail.com>
2023-07-27 15:57:17 +0200

    Moved App to ui/App

--- src/jvmMain/kotlin/com/sandstonelauncher/Main.kt
+++ src/jvmMain/kotlin/com/sandstonelauncher/Main.kt
@@ -1,21 +1,9 @@
 package com.sandstonelauncher
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.sandstonelauncher.di.configDi
-
-@Composable
-@Preview
-fun App() {
-    AppTheme {
-        Scaffold {
-
-        }
-    }
-}
+import com.sandstonelauncher.ui.App
 
 fun main() {
     configDi()
