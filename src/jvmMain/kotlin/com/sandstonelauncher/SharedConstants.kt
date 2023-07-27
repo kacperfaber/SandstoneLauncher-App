41de486211518d637eeb9b15f58bed1b40057b54
Kacper Faber <kacperf1234@gmail.com>
2023-07-27 11:54:37 +0200

    CurrentProfile, SharedConstants
    SharedConstants holds objects Profile and EnvVar - they are holding const values.
    
    CurrentProfile has val 'currentProfile: Profile' and enum Profile declaration.

---
+++ src/jvmMain/kotlin/com/sandstonelauncher/SharedConstants.kt
@@ -0,0 +1,13 @@
+package com.sandstonelauncher
+
+object SharedConstants {
+    object Profiles {
+        const val TEST = "TEST"
+        const val DEV = "DEV"
+        const val PROD = "PROD"
+    }
+
+    object EnvVar {
+        const val CURRENT_PROFILE = "sandstone.profile"
+    }
+}
