f095c0f316ef27aae2704b2c9a8ab69512cca7c4
Kacper Faber <kacperf1234@gmail.com>
2023-07-27 16:42:50 +0200

    DevConsoleApiWrapper.
    Added some test data

--- src/jvmMain/kotlin/com/sandstonelauncher/services/consoleapi/wrapper/DevConsoleApiWrapper.kt
+++ src/jvmMain/kotlin/com/sandstonelauncher/services/consoleapi/wrapper/DevConsoleApiWrapper.kt
@@ -1,9 +1,13 @@
 package com.sandstonelauncher.services.consoleapi.wrapper
 
 import com.sandstonelauncher.data.LauncherProfile
+import com.sandstonelauncher.data.Resolution
 
 class DevConsoleApiWrapper : ConsoleApiWrapper {
-    private var profiles = mutableListOf<LauncherProfile>()
+    private var profiles = mutableListOf(
+        LauncherProfile("kacperek", "%APPDATA%\\.minecraft", "C:\\Users\\kacperfaber", "", Resolution(854, 480), "1.19.2"),
+        LauncherProfile("mateuszek", "%APPDATA%\\.minecraft", "C:\\Users\\kacperfaber", "", Resolution(854, 480), "1.12")
+    )
 
     override suspend fun listProfiles(): List<LauncherProfile> {
         return profiles
