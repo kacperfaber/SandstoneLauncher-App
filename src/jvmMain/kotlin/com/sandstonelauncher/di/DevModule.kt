522233e329ff57cef94635a960262f1c44aa895b
Kacper Faber <kacperf1234@gmail.com>
2023-07-27 16:42:24 +0200

    DevModule, ProdModule was not binding interface.

--- src/jvmMain/kotlin/com/sandstonelauncher/di/DevModule.kt
+++ src/jvmMain/kotlin/com/sandstonelauncher/di/DevModule.kt
@@ -1,8 +1,10 @@
 package com.sandstonelauncher.di
 
+import com.sandstonelauncher.services.consoleapi.wrapper.ConsoleApiWrapper
 import com.sandstonelauncher.services.consoleapi.wrapper.DevConsoleApiWrapper
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal fun devModule() = module {
-    single { DevConsoleApiWrapper() }
+    single { DevConsoleApiWrapper() } bind ConsoleApiWrapper::class
 }
