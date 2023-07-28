7959b46e5bf742792dab5f70d625ebe8787e7233
Kacper Faber <kacperf1234@gmail.com>
2023-07-28 11:01:51 +0200

    Implemented AppBar.

--- src/jvmMain/kotlin/com/sandstonelauncher/Main.kt
+++ src/jvmMain/kotlin/com/sandstonelauncher/Main.kt
@@ -1,16 +1,33 @@
 package com.sandstonelauncher
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.sandstonelauncher.di.configDi
 import com.sandstonelauncher.ui.App
+import com.sandstonelauncher.ui.components.appbar.AppBar
 
 fun main() {
     configDi()
 
     application {
-        Window(onCloseRequest = ::exitApplication) {
-            App()
+        val windowState = rememberWindowState()
+
+        Window(onCloseRequest = ::exitApplication, undecorated = true, state = windowState) {
+            AppTheme {
+                Column {
+                    WindowDraggableArea {
+                        AppBar(
+                            exit = {exitApplication()},
+                            minimize = {windowState.isMinimized = true}
+                        )
+                    }
+
+                    App()
+                }
+            }
         }
     }
 }
