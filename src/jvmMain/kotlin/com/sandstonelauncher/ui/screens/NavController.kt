c0ae299d94296f0615b64cc2d5ab9519cc392ac0
Kacper Faber <kacperf1234@gmail.com>
2023-07-28 11:34:45 +0200

    NavController - Introducing back stack entry.
    I have private list of BackStackEntry objects.
    
    when navigateUp() called, we're loading last entry item, and removing this item.

--- src/jvmMain/kotlin/com/sandstonelauncher/ui/screens/NavController.kt
+++ src/jvmMain/kotlin/com/sandstonelauncher/ui/screens/NavController.kt
@@ -5,10 +5,27 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 
 class NavController {
+    class BackStackEntry(val screen: Screen, val args: Map<String, String?>?)
+
     var currentScreen = mutableStateOf(Screen.Home)
     private var currentArgs = mutableStateOf<Map<String, String?>?>(null)
+    private var backStack = mutableListOf<BackStackEntry>()
+
+    private fun registerBackStackEntry() {
+        backStack.add(BackStackEntry(currentScreen.value, currentArgs.value))
+    }
+
+    private fun dropLastStackEntry(): BackStackEntry {
+        return backStack.removeLast()
+    }
+
+    private fun loadBackStackEntry(entry: BackStackEntry) {
+        currentScreen.value = entry.screen
+        currentArgs.value = entry.args
+    }
 
     fun navigate(screen: Screen) {
+        registerBackStackEntry()
         currentScreen.value = screen
         currentArgs.value = null
     }
@@ -20,6 +37,11 @@ class NavController {
 
     fun getStringArg(key: String): String? {
         return currentArgs.value?.get(key)
+    }
+
+    fun navigateUp() {
+        val lastEntry = dropLastStackEntry()
+        loadBackStackEntry(lastEntry)
     }
 }
 
